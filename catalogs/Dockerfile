3db164ad444d5c320a57aee92aa0393e06744981
Hugo Brilhante <hugobrilhante@gmail.com>
2021-03-10 18:21:36 -0300

    Adjusts globals

--- catalogs/Dockerfile
+++ catalogs/Dockerfile
@@ -4,3 +4,4 @@ WORKDIR /code
 COPY requirements.txt /code/
 RUN pip install -r requirements.txt
 COPY . /code/
+
--- marketplace/marketplace/asgi.py
+++ marketplace/marketplace/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for users project.
+ASGI config for marketplace project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'users.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'marketplace.settings')
 
 application = get_asgi_application()
